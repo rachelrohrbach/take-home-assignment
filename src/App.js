c5cb836101296edcfe349a8035cf0e5ec883afaa
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-03-10 18:37:18 -0800

    Remove unneccessary code comments and clean up code

--- src/App.js
+++ src/App.js
@@ -17,52 +17,39 @@ function App() {
     transformText(textInput);
   };
 
-  //Lines should not exceed 80 characters
-  // If the 81st char is the middle of the word, break the line on the closest previous space
-  // If a single word exceeds 80 characters, leave that word intact on a line by itself (this is an exception to the 80-character-per-line limit).
-  // One blank line between paragraphs.
-  // No more than one consecutive space or blank line in the formatted text -- collapse multiples into a single space or line.
-
   const transformText = input => {
-    // let output = input;
-    console.log(input.length);
     const maxLineLength = 80;
-
     let output = '';
 
-    let paragraphs = input.split('\n\n');
-    console.log(paragraphs);
+    const paragraphs = formParagraphs(input);
     for (const paragraph of paragraphs) {
-      let words = paragraph.split(/\s+/);
-      console.log(words);
+      const words = formWords(paragraph);
       let charCount = 0;
 
-      for (let i = 0; i < words.length; i++) {
-        if (words[i].length > maxLineLength) {
-          output += words[i] + ' \n';
+      for (const word of words) {
+        if (word.length > maxLineLength) {
+          //if single word exceeds 80 characters
+          output += word + ' \n';
         } else {
-          if (charCount + words[i].length < maxLineLength) {
-            output += words[i] + ' ';
-            charCount += words[i].length + 1;
-
-          } else {
+          if (charCount + word.length >= maxLineLength) {
             charCount = 0;
             output += '\n';
-
-            output += words[i] + ' ';
-            charCount += words[i].length + 1;
-
           }
+          output += word + ' ';
+          charCount += word.length + 1;
         }
       }
       output += '\n\n';
     }
-
-    /*
-    your work goes here!
-    */
     setTextOutput(output);
-    // "someone's in the house\n\ntwo"
+  };
+
+  const formParagraphs = entireText => {
+    return entireText.split('\n\n');
+  };
+
+  const formWords = paragraph => {
+    return paragraph.trim().split(/\s+/);
   };
 
   return (
